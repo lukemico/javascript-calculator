bf9d8fefe9da518aced3a9403138da7266a54aa8
Luke Mico <lukemico@me.com>
2019-11-23 10:27:36 +1100

    implement Returning Values from a function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+	const result = num1 + num2;
+	alert("The result is " + result);
+	return result;
+}
+
+currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
