0d0e876caf49e60c950c74508593a806e6b9fadc
Luke Mico <lucasm1603@gmail.com>
2020-09-13 12:33:04 +1000

    implement Game User Choice

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function() {
-//   console.log('Game is starting...');
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-// const person = {
-//   name: 'Max',
-//   greet: function greet() {
-//     console.log('Hello there!');
-//   }
-// };
+const getPlayerChoice = function () {
+	const selection = prompt(
+		`${ROCK}, ${PAPER} or ${SCISSORS}?`,
+		''
+	).toUpperCase();
+	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+		alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+		return DEFAULT_USER_CHOICE;
+	}
+	return selection;
+};
 
-// person.greet();
-
-// console.dir(startGame);
-
-startGameBtn.addEventListener('click', function() {
-  console.log('Game is starting...');
+startGameBtn.addEventListener('click', function () {
+	console.log('Game is starting...');
+	const playerSelection = getPlayerChoice();
+	console.log(playerSelection);
 });
