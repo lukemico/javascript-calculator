7678c0099bc8911d2e2fbd643fd0912a523f86b4
Luke Mico <lucasm1603@gmail.com>
2020-09-13 12:37:24 +1000

    implement Game Logic

--- app.js
+++ app.js
@@ -11,36 +11,36 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 let gameIsRunning = false;
 
 const getPlayerChoice = () => {
-	const selection = prompt(
-		`${ROCK}, ${PAPER} or ${SCISSORS}?`,
-		''
-	).toUpperCase();
-	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-		alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-		return DEFAULT_USER_CHOICE;
-	}
-	return selection;
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
 
 const getComputerChoice = () => {
-	const randomValue = Math.random();
-	if (randomValue < 0.34) {
-		return ROCK;
-	} else if (randomValue < 0.67) {
-		return PAPER;
-	} else {
-		return SCISSORS;
-	}
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
 };
 
 const getWinner = (cChoice, pChoice) =>
-	cChoice === pChoice
-		? RESULT_DRAW
-		: (cChoice === ROCK && pChoice === PAPER) ||
-		  (cChoice === PAPER && pChoice === SCISSORS) ||
-		  (cChoice === SCISSORS && pChoice === ROCK)
-		? RESULT_PLAYER_WINS
-		: RESULT_COMPUTER_WINS;
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
 
 // if (cChoice === pChoice) {
 //   return RESULT_DRAW;
@@ -55,13 +55,22 @@ const getWinner = (cChoice, pChoice) =>
 // }
 
 startGameBtn.addEventListener('click', () => {
-	if (gameIsRunning) {
-		return;
-	}
-	gameIsRunning = true;
-	console.log('Game is starting...');
-	const playerChoice = getPlayerChoice();
-	const computerChoice = getComputerChoice();
-	const winner = getWinner(computerChoice, playerChoice);
-	console.log(winner);
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therefore you `;
+  if (winner === RESULT_DRAW) {
+    message = message + 'had a draw.';
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + 'won.';
+  } else {
+    message = message + 'lost.';
+  }
+  alert(message);
+  gameIsRunning = false;
 });
